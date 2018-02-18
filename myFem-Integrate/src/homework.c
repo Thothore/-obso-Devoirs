167100336248602c66692ca6797707abef2d9062
Thothore <35405712+Thothore@users.noreply.github.com>
2018-02-18 15:20:59 +0100

    Add files via upload

--- myFem-Integrate/src/homework.c
+++ myFem-Integrate/src/homework.c
@@ -9,9 +9,18 @@
 
 double integrate(double x[3], double y[3], double (*f) (double, double))
 {
-    double I = sqrt(2);
-    double xLoc[3];
-    double yLoc[3];
+    double I = 0;
+	double xLoc[3] = { 1.0 / 6.0, 1.0 / 6.0, 2.0 / 3.0 };
+	double yLoc[3] = { 1.0 / 6.0, 2.0 / 3.0, 1.0 / 6.0 };
+	double wLoc[3] = { 1.0 / 6.0, 1.0 / 6.0, 1.0 / 6.0 };
+	int i;
+	for (i = 0; i < 3; i++)
+	{
+		double X = x[0] * (1 - xLoc[i] - yLoc[i]) + x[1] * (xLoc[i]) + x[2] * (yLoc[i]);
+		double Y = y[0] * (1 - xLoc[i] - yLoc[i]) + y[1] * (xLoc[i]) + y[2] * (yLoc[i]);
+		double W = wLoc[0] * (1 - xLoc[i] - yLoc[i]) + wLoc[1] * (xLoc[i]) + wLoc[2] * (yLoc[i]);
+		I = I + W * f(X, Y);
+	}
 
 //
 // ... A modifier :-)
@@ -26,7 +35,7 @@ double integrate(double x[3], double y[3], double (*f) (double, double))
 
     glColor3f (1.0,1.0,1.0); glfemDrawElement(x,y,3);
     glColor3f (1.0,0.0,0.0); glfemDrawNodes(x,y,3);
-//    glColor3f (0.0,0.0,1.0); glfemDrawNodes(xLoc,yLoc,3);
+    glColor3f (0.0,0.0,1.0); glfemDrawNodes(xLoc,yLoc,3);
     
 #endif
 
@@ -40,8 +49,7 @@ double integrateRecursive(double x[3], double y[3], double (*f)(double,double), 
 // ... A modifier :-)
 // y-compris la ligne juste en dessous :-)
 //
-    double I = integrate(x,y,f);
-    
+	double I = integrate(x, y, f);
 //
 //
 //    
